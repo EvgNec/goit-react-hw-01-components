360b8d54b2c1ff26227af0e7b173db3b86d7f78c
Evgeniy <evgnec@gmail.com>
2023-05-21 14:39:22 +0300

    add ex1

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -23,11 +23,11 @@ export default function ProfileCard({ username, tag, location, avatar = defaulIm
       <span className={css.label}>Followers</span>
       <span className={css.quantity}>{stats.followers}</span>
     </li>
-    <li>
+    <li className={css.item}>
       <span className={css.label}>Views</span>
       <span className={css.quantity}>{stats.views}</span>
     </li>
-    <li>
+    <li className={css.item}>
       <span className={css.label}>Likes</span>
       <span className={css.quantity}>{stats.likes}</span>
     </li>
